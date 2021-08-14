4e73ed050c7ca8616909881bb727c08299acdeed
ivanburda1986 <burda.ivan@gmail.com>
2021-08-14 17:39:58 +0200

    cards-page

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { getIconComponent } from "../../utils/getIconComponent";
 import CSS_COLOR_NAMES from "../../utils/cssColorNames";
 
 import Classes from "./ProductCard.module.css";
+import { product } from "prelude-ls";
 
 export default function ProductCard({ item, bgColor }) {
   const productCardData = { ...item };
+  console.log(productCardData);
   const { icon, title } = productCardData;
 
   let cssColors = CSS_COLOR_NAMES.map((color) => color.toLowerCase());
@@ -13,11 +16,13 @@ export default function ProductCard({ item, bgColor }) {
 
   return (
     <div className={Classes.ProductCardWrapper}>
-      <div className={Classes.ProductCard} style={{ backgroundColor: `${cardBackgroundColor}` }}>
-        <div className={Classes.Icon}>{getIconComponent({ icon: icon, color: "white", fontSize: "2.5rem", alignSelf: "left" })}</div>
-        <p className={Classes.InnerTitle}>{title}</p>
-      </div>
-      <p className={Classes.OuterTitle}>{title}</p>
+      <Link to={productCardData.action ? productCardData.action.target : null}>
+        <div className={Classes.ProductCard} style={{ backgroundColor: `${cardBackgroundColor}` }}>
+          <div className={Classes.Icon}>{getIconComponent({ icon: icon, color: "white", fontSize: "2.5rem", alignSelf: "left" })}</div>
+          <p className={Classes.InnerTitle}>{title}</p>
+        </div>
+        <p className={Classes.OuterTitle}>{title}</p>
+      </Link>
     </div>
   );
 }
