ad0042e13ef1cc6dca195adae82794ace84dc6c5
ivanburda1986 <burda.ivan@gmail.com>
2021-08-14 14:21:04 +0200

    cleanup folder structure

--- src/contexts/storeMainData.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const StoreMainDataContext = React.createContext();
-
-export const StoreMainDataConsumer = StoreMainDataContext;
-export const StoreMainDataProvider = StoreMainDataContext.Provider;
