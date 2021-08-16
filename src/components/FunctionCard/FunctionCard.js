91fdd56e8f7534caa8c2af2d1213471da1be6190
ivanburda1986 <burda.ivan@gmail.com>
2021-08-16 19:51:26 +0200

    functioncard

--- src/components/FunctionCard/FunctionCard.js
+++ src/components/FunctionCard/FunctionCard.js
@@ -1,23 +1,20 @@
-import React from 'react'
-import Classes from './FunctionCard.module.css';
-import {BASE_URL} from '../../utils/api';
+import React from "react";
+import Classes from "./FunctionCard.module.css";
+import { BASE_URL } from "../../utils/api";
 
-export default function FunctionCard({item}) {
+export default function FunctionCard({ item }) {
   const functionCardData = { ...item };
   const { productImage, title, subtitle } = functionCardData;
   console.log(`${BASE_URL}${productImage.formats.thumbnail.url}`);
   return (
     <div className={Classes.FunctionCardWrapper}>
       <div className={Classes.FunctionCard}>
-        <div className={Classes.Image} style={{ backgroundImage: `url(${BASE_URL}${productImage.url})`}}></div>
-        <div>
-        <p className={Classes.Title}>{title}</p>
-        <p className={Classes.Subtitle}>
-          {subtitle.slice(0, 40)}
-          {subtitle.length > 40 ? "..." : null}
-        </p>
+        <div className={Classes.Image} style={{ backgroundImage: `url(${BASE_URL}${productImage.url})` }}></div>
+        <div className={Classes.Body}>
+          <p className={Classes.Title}>{title}</p>
+          <p className={Classes.Subtitle}>{subtitle}</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
